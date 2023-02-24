47164f0d599d9e20b182cd79f78d93323ea21e6b
kirillsev1 <kirillprinh47@gmail.com>
2023-02-24 10:49:35 +0300

    add buttons

--- main.py
+++ main.py
@@ -17,6 +17,22 @@ class MoodCalendar:
         self.mood_frame = tk.Frame(master, width=200, height=200)
         self.mood_frame.pack(side=tk.TOP)
 
+        self.happy_button = tk.Button(self.mood_frame, text="Happy", command=self.add_mood_happy)
+        self.happy_button.pack(side=tk.LEFT)
+
+        self.sad_button = tk.Button(self.mood_frame, text="Sad", command=self.add_mood_sad)
+        self.sad_button.pack(side=tk.LEFT)
+
+        self.neutral_button = tk.Button(self.mood_frame, text="Neutral", command=self.add_mood_neutral)
+        self.neutral_button.pack(side=tk.LEFT)
+
+        self.stats = tk.Button(master, text="Stats", command=self.update_chart)
+        self.stats.pack(side=tk.BOTTOM)
+
+        self.mood_arr = []
+        self.moods = self.load_mood()
+        self.update_text()
+
 
 if __name__ == "__main__":
     root = tk.Tk()
